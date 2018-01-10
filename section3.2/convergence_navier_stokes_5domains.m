a85609cc4f87986cdab63e01a5e41022bc83dd57
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-01-10 14:47:58 +0100

    Modify convergence on 5 domains

--- section3.2/convergence_navier_stokes_5domains.m
+++ section3.2/convergence_navier_stokes_5domains.m
@@ -4,7 +4,7 @@ clc
 
 % author: Luca Pegolotti on 11/12/2017
 
-set = 3;
+set = 1;
 
 if (set == 1)
     U = 1;
@@ -229,238 +229,55 @@ for nx = n_elementsx
     fespace5_u = create_fespace(mesh5,'P2',bc_flags);
     fespace5_p = create_fespace(mesh5,'P1',bc_flags);
     
-    draw_multimesh({mesh1, mesh2, mesh3, mesh4, mesh5})
-    pause();
-    
-    
+%     draw_multimesh({mesh1, mesh2, mesh3, mesh4, mesh5})
+%     pause();
+
     fespaces_u = {fespace1_u,fespace2_u,fespace3_u,fespace4_u,fespace5_u};
     fespaces_p = {fespace1_p,fespace2_p,fespace3_p,fespace4_p,fespace5_p};
-    
-    % store number of degrees of freedom for one component of the velocity
-    n1u = size(fespace1_u.nodes,1);
-    n2u = size(fespace2_u.nodes,1);
-    n3u = size(fespace3_u.nodes,1);
-    n4u = size(fespace4_u.nodes,1);
-    n5u = size(fespace5_u.nodes,1);
 
-    
-    % store number of degrees of freedom for the pressure
-    n1p = size(fespace1_p.nodes,1);
-    n2p = size(fespace2_p.nodes,1);
-    n3p = size(fespace3_p.nodes,1);
-    n4p = size(fespace4_p.nodes,1);
-    n5p = size(fespace5_p.nodes,1);
-
-    n1 = 2*n1u+n1p;
-    n2 = 2*n2u+n2p;
-    n3 = 2*n3u+n3p;
-    n4 = 2*n4u+n4p;
-    n5 = 2*n5u+n5p;
- 
-    indices1 = 1:n1;
-    indices2 = n1+1:n1+n2;
-    indices3 = n1+n2+1:n1+n2+n3;
-    indices4 = n1+n2+n3+1:n1+n2+n3+n4;
-    indices5 = n1+n2+n3+n4+1:n1+n2+n3+n4+n5;
-    
-    % build matrices and righ handsides for the 5 domains
-    [A1,rhs1] = assembler_steady_navier_stokes(fespace1_u,fespace1_p,fun,nu,dirichlet_functions,neumann_functions);
-    [A2,rhs2] = assembler_steady_navier_stokes(fespace2_u,fespace2_p,fun,nu,dirichlet_functions,neumann_functions);
-    [A3,rhs3] = assembler_steady_navier_stokes(fespace3_u,fespace3_p,fun,nu,dirichlet_functions,neumann_functions);
-    [A4,rhs4] = assembler_steady_navier_stokes(fespace4_u,fespace4_p,fun,nu,dirichlet_functions,neumann_functions);
-    [A5,rhs5] = assembler_steady_navier_stokes(fespace5_u,fespace5_p,fun,nu,dirichlet_functions,neumann_functions);
-    
-    A = @(u) blkdiag(A1(u(indices1)),A2(u(indices2)),A3(u(indices3)),A4(u(indices4)),A5(u(indices5)));
-    
-    sol_appr = zeros(n1+n2+n3+n4+n5,1);
-    sollm = [];
-    
-    errsL2u = [];
-    
-    bcs_flags = [-1 0 0 0; 0 0 1 1; 0 -1 1 0;0 0 0 0;0 0 1 0]';
-    
-    bfs1 = {};
-    bfs2 = {};
-    
-    B = [];
-    B_t = [];
-    
+    domain_connectivity = [1 3 3 0 3; 0 4 2 0 0; 0 0 4 2 2; 0 0 0 3 1];
+    normals = [-1 0 0 0; 0 0 1 1; 0 -1 1 -1; 0 1 -1 0; 1 1 1 0];
     gausspoints = 4;
-    
-    for n_itx = 0:10
-        %bf = @(x) legendreP(n_itx,(x(1)-0.5)*2);
-        bf = @(x) x(1,:).^(n_itx);
-
-        %bf = @(x) cos(x(1)*pi*n_itx);
-        [B1,B1_t] = couple_navier_stokes_solutions_single_basis_xpar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-        B = [B;B1];
-        B_t = [B_t B1_t];
-        
-%         if (n_itx ~= 0)
-%             bf = @(x) sin(x(1)*pi*n_itx);
-%             [B1,B1_t] = couple_navier_stokes_solutions_single_basis_xpar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-%             B = [B;B1];
-%             B_t = [B_t B1_t];
-%         end
-    end
-    
-    for n_ity = 0:10
-        %bf = @(x) legendreP(n_ity,(x(2)-0.5)*2);
-        bf = @(x) x(2,:).^(n_ity);
-        %bf = @(x) cos(x(2)*pi*n_ity);
-        [B1,B1_t] = couple_navier_stokes_solutions_single_basis_ypar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-        B = [B;B1];
-        B_t = [B_t B1_t];
-        
-%         if (n_itx ~= 0)
-%             bf = @(x) sin(x(2)*pi*n_ity);
-%             [B1,B1_t] = couple_navier_stokes_solutions_single_basis_ypar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-%             B = [B;B1];
-%             B_t = [B_t B1_t];
-%         end
-    end
-    
-    bcs_flags = [0 0 0 0; 0 0 0 0; 0 0 0 1;0 -1 1 0;-1 -1 0 0]';
-
-    for n_itx = 0:10
-        %bf = @(x) legendreP(n_itx,(x(1)-0.5)*2);
-        bf = @(x) x(1,:).^(n_itx);
-
-        %bf = @(x) cos(x(1)*pi*n_itx);
-        [B1,B1_t] = couple_navier_stokes_solutions_single_basis_xpar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-        B = [B;B1];
-        B_t = [B_t B1_t];
-        
-%         if (n_itx ~= 0)
-%             bf = @(x) sin(x(1)*pi*n_itx);
-%             [B1,B1_t] = couple_navier_stokes_solutions_single_basis_xpar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-%             B = [B;B1];
-%             B_t = [B_t B1_t];
-%         end
-    end
-    
-    for n_ity = 0:10
-        %bf = @(x) legendreP(n_ity,(x(2)-0.5)*2);
-        bf = @(x) x(2,:).^(n_ity);
-        %bf = @(x) cos(x(2)*pi*n_ity);
-        [B1,B1_t] = couple_navier_stokes_solutions_single_basis_ypar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-        B = [B;B1];
-        B_t = [B_t B1_t];
-        
-%         if (n_itx ~= 0)
-%             bf = @(x) sin(x(2)*pi*n_ity);
-%             [B1,B1_t] = couple_navier_stokes_solutions_single_basis_ypar(fespaces_u,fespaces_p,bcs_flags,bf,gausspoints);
-%             B = [B;B1];
-%             B_t = [B_t B1_t];
-%         end
-    end
-   
-    totalagmul = size(B,1);
-    zerosp = sparse(totalagmul,totalagmul);
-    mat = @(u) [A(u) B_t; B zerosp];
-    
-    zeroslg = zeros(totalagmul,1);
+    typebasisfunctions = 'polynomial';
+    nbasisfunctions = [6 6 6 6];
+    [mat,rhs,jac,nsys,nus,nps,indices] = build_coupled_system_navier_stokes(fespaces_u,fespaces_p,fun,nu,dirichlet_functions,neumann_functions,domain_connectivity,normals,nbasisfunctions,gausspoints,typebasisfunctions);
     
     % solve system with newton's method
-    f = @(u) mat(u)*u-[rhs1;rhs2;rhs3;rhs4;rhs5;zeroslg];
-    x0 = [sol_appr; zeros(totalagmul-size(sollm,1),1)];
-    jac = @(u) [blkdiag(build_jac_navier_stokes(A1,u(indices1),fespace1_u), ...
-        build_jac_navier_stokes(A2,u(indices2),fespace2_u), ...
-        build_jac_navier_stokes(A3,u(indices3),fespace3_u), ...
-        build_jac_navier_stokes(A4,u(indices4),fespace4_u), ...
-        build_jac_navier_stokes(A5,u(indices5),fespace5_u)), B_t;
-        B zerosp];
+    f = @(u) mat(u)*u-rhs;
+    x0 = zeros(nsys,1);
     tol = 1e-10;
     maxit = 20;
     
-    [sol_apr,er,it] = solve_with_newtons_method(f,x0,jac,tol,maxit);
+    [sol,er,it] = solve_with_newtons_method(f,x0,jac,tol,maxit);
     
-    sol1 = real(sol_apr(indices1));
-    sol2 = real(sol_apr(indices2));
-    sol3 = real(sol_apr(indices3));
-    sol4 = real(sol_apr(indices4));
-    sol5 = real(sol_apr(indices5));
-
-    solm = sol_apr(n1+n2+n3+n4+n5+1:end);
+    [sols,lm] = split_solutions(sol,fespaces_u,fespaces_p,nus,nps,indices);
     
-    solstr1.u1 = sol1(1:n1u);
-    solstr1.u2 = sol1(n1u+1:n1u*2);
-    solstr1.p  = sol1(2*n1u+1:end);
-    solstr1.fespace_u = fespace1_u;
-    solstr1.fespace_p = fespace1_p;
-    
-    solstr2.u1 = sol2(1:n2u);
-    solstr2.u2 = sol2(n2u+1:n2u*2);
-    solstr2.p  = sol2(2*n2u+1:end);
-    solstr2.fespace_u = fespace2_u;
-    solstr2.fespace_p = fespace2_p;
-    
-    solstr3.u1 = sol3(1:n3u);
-    solstr3.u2 = sol3(n3u+1:n3u*2);
-    solstr3.p  = sol3(2*n3u+1:end);
-    solstr3.fespace_u = fespace3_u;
-    solstr3.fespace_p = fespace3_p;
-    
-    solstr4.u1 = sol4(1:n4u);
-    solstr4.u2 = sol4(n4u+1:n4u*2);
-    solstr4.p  = sol4(2*n4u+1:end);
-    solstr4.fespace_u = fespace4_u;
-    solstr4.fespace_p = fespace4_p;
-    
-    solstr5.u1 = sol5(1:n5u);
-    solstr5.u2 = sol5(n5u+1:n5u*2);
-    solstr5.p  = sol5(2*n5u+1:end);
-    solstr5.fespace_u = fespace5_u;
-    solstr5.fespace_p = fespace5_p;
+    % plot solutions and compute errors
     
     figure(1)
-    close all
-    plot_fe_fluid_function(solstr1,'U');
-    hold on
-    plot_fe_fluid_function(solstr2,'U');
-    plot_fe_fluid_function(solstr3,'U');
-    plot_fe_fluid_function(solstr4,'U');
-    plot_fe_fluid_function(solstr5,'U');
-
+    errsu = zeros(5,1);
+    errsp = zeros(5,1);
+    for i = 1:5
+        plot_fe_fluid_function(sols{i},'U');
+        hold on
+        errsu(i) = compute_H1_error_velocity(fespaces_u{i},sols{i},@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
+        u2exdx(x) u2exdy(x)]);
+        errsp(i) = compute_L2_error(fespaces_p{i},sols{i}.p,pex);
+    end
     axis([0 1 0 1])
-    pause(0.01)
     hold off
-   
+    pause(0.1)
+ 
+    errsH1u = sqrt(errsu'*errsu);
+    errsL2p = sqrt(errsp'*errsp);
     
-    err1 = compute_H1_error_velocity(fespace1_u,solstr1,@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
-        u2exdx(x) u2exdy(x)]);
-    err2 = compute_H1_error_velocity(fespace2_u,solstr2,@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
-        u2exdx(x) u2exdy(x)]);
-    err3 = compute_H1_error_velocity(fespace3_u,solstr3,@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
-        u2exdx(x) u2exdy(x)]);
-    err4 = compute_H1_error_velocity(fespace4_u,solstr4,@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
-        u2exdx(x) u2exdy(x)]);    
-    err5 = compute_H1_error_velocity(fespace5_u,solstr5,@(x) [u1ex(x);u2ex(x)],@(x) [u1exdx(x) u1exdy(x); ...
-        u2exdx(x) u2exdy(x)]);
-    
-    err1p = compute_L2_error(fespace1_p,solstr1.p,pex);
-    err2p = compute_L2_error(fespace2_p,solstr2.p,pex);
-    err3p = compute_L2_error(fespace3_p,solstr3.p,pex);
-    err4p = compute_L2_error(fespace4_p,solstr4.p,pex);
-    err5p = compute_L2_error(fespace5_p,solstr5.p,pex);
-      
-    errsH1u = sqrt(err1^2+err2^2+err3^2+err4^2+err5^2);
-    errsL2p = sqrt(err1p^2+err2p^2+err3p^2+err4p^2+err5p^2);
-    
-    err1 = err1 + err1p;
-    err2 = err2 + err2p;
-    err3 = err3 + err3p;
-    err4 = err4 + err4p;
-    err5 = err5 + err5p;
+    errs = errsu + errsp;
 
     errH1u = [errH1u;errsH1u]
     errL2p = [errL2p;errsL2p]
-    err = [err; sqrt(err1^2 + err2^2 + err3^2 + err4^2 + err5^2)]
+    err = [err; sqrt(errs'*errs)]
 end
 
-% loglog(h,errL2u)
-% hold on
-% loglog(h,h.^3*errL2u(1)/(h(1)^3));
 %%
 close all
 n = length(h);
