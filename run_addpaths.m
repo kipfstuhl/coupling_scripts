df59ed7ccea654852eb58c1e57704346f51ff61c
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-11-27 00:55:51 +0100

    Edit run_addpaths.m

--- run_addpaths.m
+++ run_addpaths.m
@@ -7,3 +7,11 @@ run feamat/addpaths.m
 
 % Add common folder to path
 addpath(genpath('./common/'))
+
+% set this variable to the location in which GeoPDEs has been decompressed
+geopdes_location = '';
+
+% Add geoPDEs folders
+addpath(genpath([geopdes_location,'/geopdes']));
+addpath(genpath([geopdes_location,'/geopdes_hierarchical']));
+addpath(genpath([geopdes_location,'/nurbs']));
